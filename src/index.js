9251c96ec4c799a35f025a3c5b46ca5566b7294b
shershhah <i200523@nu.edu.pk>
2023-06-24 11:12:34 +0500

    firebase connected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("HELLO JEE ");
+import { initializeApp } from "firebase/app";
+import { getDocs, getFirestore, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAB8bPh1rM53neKjWV9_LllnQcQhedpJVE",
+  authDomain: "fir-9-tutorial-b848a.firebaseapp.com",
+  projectId: "fir-9-tutorial-b848a",
+  storageBucket: "fir-9-tutorial-b848a.appspot.com",
+  messagingSenderId: "783187250852",
+  appId: "1:783187250852:web:6fadef4d346ce16ec4e031",
+};
+
+// init firebase
+initializeApp(firebaseConfig);
+
+// init firestore service
+const db = getFirestore();
+
+// // collecetion ref
+const booksRef = collection(db, "books");
+
+// // get documents
+getDocs(booksRef)
+  .then((snapshot) => {
+    let books = [];
+    snapshot.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
